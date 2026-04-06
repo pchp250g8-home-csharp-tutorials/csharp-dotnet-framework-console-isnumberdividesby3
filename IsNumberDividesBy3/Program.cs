a6dabc31f8d249617045ee394092b33d40e2923f
Alexander <pchp250g8@tutamail.com>
2026-04-06 10:25:54 +0300

    Add project files.

---
+++ IsNumberDividesBy3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsNumberDivideBy3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Input an Integer Number:");
+            var strNum = Console.ReadLine();
+            Int64.TryParse(strNum, out var iNum);
+            var nSumDigits = 0L; var iVal = Math.Abs(iNum);
+            while (iVal > 0)
+            {
+                nSumDigits += iVal % 10;
+                iVal /= 10;
+            }
+            Console.WriteLine("The sum of digits of the number {0} is {1}", iNum, nSumDigits);
+            if (nSumDigits % 3 == 0)
+            {
+                Console.WriteLine("The number {0} divides by 3", iNum);
+            }
+            else
+            {
+                Console.WriteLine("The number {0} does not divide by 3", iNum);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ IsNumberDividesBy3/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IsNumberDivideBy3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IsNumberDivideBy3")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5d716d79-820b-4f5c-8a8d-0e43feea56af")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
